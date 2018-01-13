680490fa4b79ccfe7029013075ce4f474323eb4e
MySoftheaven BD <mysoftheavenbd@mysoftheavens-mac-mini.local>
2018-01-13 16:55:08 +0600

    Initial Commit

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by MySoftheaven BD on 13/1/18.
+//  Copyright © 2018 MySoftheaven BD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
